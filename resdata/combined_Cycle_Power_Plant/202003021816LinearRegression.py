31bdfff2b8d68c8fad539f17b0146a447d68f200
yuwenxianglong <yuwenxianglong@hotmail.com>
2020-03-05 13:00:18 +0800

    9th blog: Optimization Algorithm of PyTorch.

--- resdata/combined_Cycle_Power_Plant/202003021816LinearRegression.py
+++ resdata/combined_Cycle_Power_Plant/202003021816LinearRegression.py
@@ -71,8 +71,8 @@ for epoch in range(num_epochs):
     epoches.append(epoch + 1)
     losses.append(loss.item())
 
-    print('epoch [%d/%d], Loss: %.4f, Learning Rate: %.8f'
-          % (epoch + 1, num_epochs, loss.item(), learning_rate))
+    print('epoch [%d/%d], Loss: %.4f, Learning Rate: %.8f' %
+          (epoch + 1, num_epochs, loss.item(), learning_rate))
     plt.subplot(2, 1, 1)
     plt.cla()
     plt.plot(ytarget.data.numpy(), preds.data.numpy(), 'r*',
