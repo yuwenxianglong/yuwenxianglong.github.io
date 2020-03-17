01f9d2bafe996b47a187b06ff674b31434a54a80
yuwenxianglong <yuwenxianglong@hotmail.com>
2020-03-17 17:29:18 +0800

    Add stockPrediction code.

---
+++ resdata/stockLSTM/lstmAirline202003152244.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+"""
+@Project : internationalAirlinePassengers
+@Author  : Xu-Shan Zhao
+@Filename: rnnAirline202003152244.py
+@IDE     : PyCharm
+@Time1   : 2020-03-15 22:44:34
+@Time2   : 2020/3/15 22:44
+@Month1  : 3月
+@Month2  : 三月
+"""
+
+import torch
+from torch import nn, optim
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data_csv = pd.read_csv('601600.csv', usecols=[9], encoding='gb18030')
+data_csv = data_csv.dropna()
+data_psger = data_csv.iloc[:, 0]
+# data_psger = (data_psger - data_psger.mean()) / (data_psger.max() - data_psger.min())
+
+input_size = 7
+
+df = pd.DataFrame()
+for i in range(input_size):
+    df['c%d' % i] = data_psger.tolist()[i: -input_size + i]
+
+df.at[len(data_psger) - input_size] = data_psger.tolist()[-input_size:]
+
+x = torch.FloatTensor(df.iloc[:, :].to_numpy())
+x = x.unsqueeze(0)
+y = data_psger[input_size - 1:]
+y = torch.FloatTensor(y.to_numpy())
+y = y.unsqueeze(0).unsqueeze(2)
+
+n_epoches = 3000
+learning_rate = 0.01
+
+epoches = []
+pres = []
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.rnn = nn.RNN(
+            input_size=input_size,
+            hidden_size=2,
+            num_layers=1,
+        )
+        self.out = nn.Linear(2, 1)
+
+    def forward(self, x, h):
+        out, h = self.rnn(x, h)
+        s, b, h = out.size()
+        out = out.view(s * b, h)
+        prediction = self.out(out)
+        prediction = prediction.view(s, b, -1)
+        return prediction, h
+
+
+model = Net()
+
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+loss_func = nn.MSELoss()
+h_state = None
+
+plt.figure()
+plt.ion()
+plt.pause(1)
+
+for epoch in range(n_epoches):
+    prediction, h = model(x, h_state)
+    loss = loss_func(prediction, y)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+    if (epoch + 1) % 1000 == 0:
+        print(epoch, loss.item())
+        plt.cla()
+        plt.plot(torch.arange(df.shape[0]).numpy(), y.view(-1).data.numpy(), 'ro')
+        plt.plot(torch.arange(df.shape[0]).numpy(), prediction.view(-1).data.numpy(), 'b-')
+        plt.draw()
+        plt.pause(0.1)
+
+plt.ioff()
+plt.show()
