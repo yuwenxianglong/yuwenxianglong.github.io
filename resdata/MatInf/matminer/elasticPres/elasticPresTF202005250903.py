002b6b7b7ed37871696a43e9373e54ad5ac208c9
yuwenxianglong <yuwenxianglong@hotmail.com>
2020-05-26 15:50:58 +0800

    43rd blog, Bulk modulus prediction using TensorFlow.

--- resdata/MatInf/matminer/elasticPres/elasticPresTF202005250903.py
+++ resdata/MatInf/matminer/elasticPres/elasticPresTF202005250903.py
@@ -39,24 +39,127 @@ except OSError:
         ]
     )
 
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.00001)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 model.compile(optimizer=optimizer, loss='mse',
               metrics=['mae', 'mse'])
 
-history = model.fit(X_train, y_train, epochs=1000,
+history = model.fit(X_train, y_train, epochs=5000,
                     batch_size=128, validation_split=0.2)
 model.save('elasticPres.h5')
 
 hist = pd.DataFrame(history.history, index=history.epoch)
-hist['loss'].plot(legend=True, style='-')
-hist['val_loss'].plot(legend=True, style=":")
-plt.show()
 
-plt.plot(y_train, model.predict(X_train), 'ro')
-plt.plot(y_train, y_train, 'k-')
-plt.axis('equal')
-plt.show()
-plt.plot(y_test, model.predict(X_test), 'ro')
-plt.plot(y_test, y_test, 'k-')
-plt.axis('equal')
-plt.show()
+import plotly.express as px
+import plotly.offline as offline
+import plotly.graph_objects as go
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=hist.index, y=hist['loss'],
+                         mode='lines+markers',
+                         name='Loss'))
+fig.add_trace(go.Scatter(x=hist.index, y=hist['val_loss'],
+                         mode='lines+markers',
+                         name='Val_Loss'))
+fig.update_layout(
+    title='Loss functions',
+    xaxis_title='Epochs',
+    yaxis_title='Loss Function',
+    font=dict(
+        family='Times New Roman',
+        size=18,
+        color='Maroon'
+    ),
+    legend=dict(
+        x=0.25,
+        y=0.9,
+        bordercolor='yellow',
+        borderwidth=2
+    )
+)
+# fig.show()
+offline.plot(fig, filename='Loss.html', auto_open=True)
+
+fig1 = go.Figure()
+m = model.predict(X_train)
+fig1.add_trace(
+    go.Scatter(
+        x=y_train,
+        y=m.reshape(len(m)),
+        mode='markers',
+        name='Bulk Prediction of Training Data'
+    )
+)
+fig1.add_trace(go.Scatter(
+    x=y_train, y=y_train,
+    mode='lines',
+    name='Y=X'
+))
+fig1.update_layout(
+    title='Bulk Prediction of Training Data',
+    xaxis_title='DFT (MP) Bulk Modulus',
+    yaxis_title='Predicted Bulk Modulus',
+    font=dict(
+        family='Times New Roman',
+        size=20,
+        color='#8B0A50'
+    ),
+    height=1000,
+    width=1000,
+    legend=dict(
+        x=0.1,
+        y=0.9,
+        bordercolor='yellow',
+        borderwidth=2
+    )
+)
+offline.plot(fig1, filename='BulkPredictionOfTrainingData.html',
+             auto_open=True)
+
+fig2 = go.Figure()
+m = model.predict(X_test)
+fig2.add_trace(
+    go.Scatter(
+        x=y_test,
+        y=m.reshape(len(m)),
+        mode='markers',
+        name='Bulk Prediction of Training Data'
+    )
+)
+fig2.add_trace(go.Scatter(
+    x=y_test, y=y_test,
+    mode='lines',
+    name='Y=X'
+))
+fig2.update_layout(
+    title='Bulk Prediction of Test Data',
+    xaxis_title='DFT (MP) Bulk Modulus',
+    yaxis_title='Predicted Bulk Modulus',
+    font=dict(
+        family='Times New Roman',
+        size=20,
+        color='#8B658B'
+    ),
+    height=1000,
+    width=1000,
+    legend=dict(
+        x=0.1,
+        y=0.9,
+        bordercolor='yellow',
+        borderwidth=2,
+        bgcolor='Cyan'
+    )
+)
+offline.plot(fig2, filename='BulkPredictionOfTestData.html',
+             auto_open=True)
+# hist['loss'].plot(legend=True, style='-')
+# hist['val_loss'].plot(legend=True, style=":")
+# plt.show()
+#
+# plt.plot(y_train, model.predict(X_train), 'ro')
+# plt.plot(y_train, y_train, 'k-')
+# plt.axis('equal')
+# plt.show()
+# plt.plot(y_test, model.predict(X_test), 'ro')
+# plt.plot(y_test, y_test, 'k-')
+# plt.axis('equal')
+# plt.show()
